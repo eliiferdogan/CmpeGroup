e58d7f3fd9da812cb14c44297dbae950f1f49815
student <dilara.celebi@bilgiedu.net>
2016-04-13 15:41:10 +0300

    Added Parse

--- Project1.rkt
+++ Project1.rkt
@@ -32,3 +32,58 @@
        ;La  -> listOfSymbols
        ;Fs -> symbol
 
+(define-type ExprC
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : symbol) (arg : (listof ExprC))]
+  [plusC (l : ExprC) (r : ExprC)]
+  [subC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [igz (exp1 : ExprC) (exp2 : ExprC) (exp3 : ExprC)]
+  [factC (x : number)]
+  [factaccC (x : number) (acc : number)]
+  [fibC (x : number)]
+  [fibaccC (x : number) (acc : number)]
+  )
+;Extended data definition ,,Function data definition
+;; Function Definition with multiple parameters. 
+(define-type FunDefC
+  [fdC (name : symbol) (arg : (listof symbol))  (body : ExprC)])
+
+;; parse : s-exp -> ExprC
+;; Purpose : To parse given s-exp to ExprC form
+
+(define (parse [s :  (listof s-expression)]) : ExprC
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-symbol? s) (idC (s-exp->symbol s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)]) 
+       (cond
+         [(= (length sl) 4)
+          (if (symbol=? 'igz (s-exp->symbol (first sl)))
+              (igz (parse (second sl))
+                       (parse (third sl))
+                       (parse (fourth sl)))
+              (error 'parse "invalid expression as input"))]
+         [(= (length sl) 3)
+          (case (s-exp->symbol (first sl))
+            [(+) (plusC (parse (second sl)) (parse (third sl)))]
+            [(*) (multC (parse (second sl)) (parse (third sl)))]
+            [(-) (subC (parse (second sl)) (parse (third sl)))]
+            [else (error 'parse "invalid list input")]
+            )]
+         [(= (length sl) 2)
+          (appC (s-exp->symbol (first sl)) (parse (second sl)))]
+         [else (error 'parse "invalid list input")])
+       )]
+    [else (error 'parse "invalid input")]))
+
+; Parser Tests :
+(test (parse (number->s-exp 5))(numC 5))
+(test (parse (symbol->s-exp 'x))(idC 'x))
+(test (parse '(+ 3 4))(plusC (numC 3)(numC 4)))
+(test (parse '(* 3 4))(multC (numC 3)(numC 4)))
+(test (parse '(+ x x))(plusC (idC 'x)(idC 'x)))
+(test (parse '(* x x))(multC (idC 'x)(idC 'x)))
+(test (parse '(f (* x x)))(appC 'f (multC (idC 'x)(idC 'x))))
