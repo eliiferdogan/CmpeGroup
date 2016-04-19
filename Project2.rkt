a134bd36b900b3083e5914b0d456de42d45a91da
igurkan <inanc.gurkan@yahoo.com>
2016-04-19 16:44:26 +0300

    Added unparse

--- Project2.rkt
+++ Project2.rkt
@@ -42,3 +42,25 @@
 (test (parse '(x y)) (λ-app (λ-var 'x) (λ-var 'y)))
 (test (parse '(λ x y)) (λ-fnc 'x (λ-var 'y)))
 (test (parse '((λ x y) (x y))) (λ-app (λ-fnc 'x (λ-var 'y)) (λ-app (λ-var 'x) (λ-var 'y))))
+
+;; Examples
+;; (λ-var 'x) -> 'x
+;; (λ-app (λ-var 'x) (λ-var 'y)) -> '(x y)
+;; (λ-fnc 'x (λ-var 'y)) -> '(λ x y)
+;; (λ-app (λ-fnc 'x (λ-var 'y)) (λ-app (λ-var 'x) (λ-var 'y))) -> '( (λ x y) (x y) )
+;; Template
+;; (define (unparse [lmbd : λ]) : s-expression
+;;   (type-case λ lmbd
+;;     [λ-var (name) (... name)]
+;;     [λ-fnc (name expr) (list->s-exp (list (... 'λ) (... name) (... expr)))]
+;;     [λ-app (lhs rhs) (list->s-exp (list (... lhs) (... rhs)))]))
+(define (unparse [lmbd : λ]) : s-expression
+  (type-case λ lmbd
+    [λ-var (name) (symbol->s-exp name)]
+    [λ-fnc (name expr) (list->s-exp (list (symbol->s-exp 'λ) (symbol->s-exp name) (unparse expr)))]
+    [λ-app (lhs rhs) (list->s-exp (list (unparse lhs) (unparse rhs)))]))
+;; Unparse Tests
+(test (unparse (λ-var 'x)) (symbol->s-exp 'x))
+(test (unparse (λ-app (λ-var 'x) (λ-var 'y))) '(x y))
+(test (unparse (λ-fnc 'x (λ-var 'y))) '(λ x y))
+(test (unparse (λ-app (λ-fnc 'x (λ-var 'y)) (λ-app (λ-var 'x) (λ-var 'y))))'((λ x y) (x y)))
